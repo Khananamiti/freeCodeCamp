b42b87954eff0ffb77633e0186d0e2d14b5ed307
Khananamiti <khananova_kha@mail.ru>
2023-12-07 18:01:05 +0600

    Заготовки слов

---
+++ 37word-blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "I love my " + myAdjective + " and cool " + myNoun + ", who " + myAdverb + " " + myVerb + " towards me."; // Change this line
+// Only change code above this line
